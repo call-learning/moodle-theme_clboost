7a1115323a60bda4387de5fd63399a48935e0693
Laurent David <lmedavid@gmail.com>
2021-06-28 13:16:24 +0200

    Update version

--- version.php
+++ version.php
@@ -24,9 +24,9 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2020092302; /* This is the version number to increment when changes needing an update are made */
+$plugin->version   = 2020092303; /* This is the version number to increment when changes needing an update are made */
 $plugin->requires  = 2019111801;
-$plugin->release   = '0.1.0';
+$plugin->release   = '0.1.1';
 $plugin->maturity  = MATURITY_ALPHA;
 $plugin->component = 'theme_clboost';
 $plugin->dependencies = [
